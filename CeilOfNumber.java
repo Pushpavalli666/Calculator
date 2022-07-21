7e80a07da5f9f8be7f03d335e2a1a3132b91dd0c
Pushpavalli Naganaboyina <Pushpavalli_Naganaboyina@epam.com>
2022-07-21 18:29:41 +0530

    CeilOfNumber.java file added in dev branch

---
+++ CeilOfNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+class CeilOfNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextDouble();
+		
+		System.out.println(Math.ceil(a));
+	}
+}
