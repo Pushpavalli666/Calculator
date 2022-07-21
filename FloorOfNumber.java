38f004c1de932b4884c281ec8df0b186a4a91e43
Pushpavalli Naganaboyina <Pushpavalli_Naganaboyina@epam.com>
2022-07-21 18:28:52 +0530

    FloorOfNumber.java file added in dev branch

---
+++ FloorOfNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+class FloorOfNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextDouble();
+		
+		System.out.println(Math.floor(a));
+	}
+}
